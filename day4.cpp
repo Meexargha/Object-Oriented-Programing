814f70bd27cf7e9fec2e3800ecd075251c155db4
Argha kundu <128323651+Meexargha@users.noreply.github.com>
2025-09-16 23:29:35 +0530

    day4.cpp

---
+++ day4.cpp
@@ -0,0 +1,31 @@
+class Hero {
+    
+    //properties
+    private:
+    int health;
+
+    public:
+    char *name;
+    char level;
+int getHealth() {
+        return health;
+    }
+
+    char getLevel() {
+        return level;
+    }
+
+    void setHealth(int h) {
+        health = h;
+    }
+
+    void setLevel(char ch) {
+        level = ch;
+    }
+
+
+Hero a;
+    a.setHealth(80);
+    a.setLevel('B');
+    cout << "level is  " << a.level << endl;
+    cout << " health is " << a.getHealth() << endl;
